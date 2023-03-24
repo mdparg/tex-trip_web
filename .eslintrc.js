3112e0b2d864fc147283a2e80210d015181bce33
Marcos Pecorari <marcos.pecorari.arg@gmail.com>
2023-03-24 20:17:36 -0300

    Antes de formatear

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+export const env = {
+  browser: true,
+  es2021: true
+};
+export const extends = 'standard';
+export const overrides = [];
+export const parserOptions = {
+  ecmaVersion: 'latest',
+  sourceType: 'module'
+};
+export const rules = {};
